cdfa02fadba8c15cb73856a88aeb9d00c2f6230c
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2026-03-22 23:26:04 +0700

    retry fetch to awake cold start

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-import qs from "qs";
 import AboutSection from "@/components/about-section-";
 import ContactSection from "@/components/contact-section";
 import Footer from "@/components/footer";
@@ -8,112 +6,18 @@ import HighlightsSection from "@/components/highlights-section";
 import ImageGallery from "@/components/image-gallery";
 import NavBar from "@/components/nav-bar";
 import PromotionSection from "@/components/promotion-section";
-import { flattenAttributes } from "@/lib/utils";
+import { getMetadata, getPageData } from "@/lib/utils";
+import type { Metadata } from "next";
 
 export const revalidate = 60;
 
 export async function generateMetadata(): Promise<Metadata> {
-  const defaultMetadata: Metadata = {
-    title: "ศรีสุราษฎร์ บ้านน็อคดาวน์",
-    description:
-      "รับสร้างบ้านน็อคดาวน์โครงเหล็ก ราคาเริ่มต้นเพียง 90,000 บาท ออฟฟิศสำเร็จรูป รับต่อเติม รีโนเวท",
-  };
-
-  try {
-    const res = await fetch(
-      `${process.env.STRAPI_API_URL}/api/global?populate[0]=Seo&populate[1]=logo&fields[0]=name&populate[2]=heroImage`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.TOKEN}`,
-        },
-      },
-    );
-
-    if (!res.ok) throw new Error(`Strapi error: ${res.status}`);
-
-    const json = await res.json();
-    const seo = json.data.Seo;
-    const faviconUrl = json.data.logo.url;
-
-    return {
-      title: seo.metaTitle,
-      description: seo.metaDescription,
-      icons: [{ url: faviconUrl }],
-      metadataBase: new URL(`${process.env.DOMAIN_NAME}`),
-      alternates: { canonical: "/" },
-      robots: "index, follow",
-      openGraph: {
-        title: seo.metaTitle,
-        description: seo.metaDescription,
-        url: process.env.DOMAIN_NAME,
-        siteName: json.data.name,
-        images: [
-          {
-            url: `${process.env.DOMAIN_NAME}/api/og`,
-            width: 1200,
-            height: 630,
-            alt: "Website Preview",
-          },
-        ],
-        type: "website",
-      },
-    };
-  } catch (error) {
-    console.error("Failed to load metadata:", error);
-    return defaultMetadata;
-  }
+  return await getMetadata();
 }
-
-const getPageData = async () => {
-  try {
-    const baseUrl = process.env.STRAPI_API_URL ?? "http://127.0.0.1:1337";
-
-    const query = qs.stringify(
-      {
-        populate: {
-          introSection: true,
-          Seo: true,
-          introImages: {
-            fields: ["url", "alternativeText", "width", "height", "name"],
-          },
-          promoBanner: { fields: ["url", "alternativeText"] },
-          aboutUsImage: { fields: ["url"] },
-          contactImage: { fields: ["url"] },
-          heroImage: { fields: ["url"] },
-          logo: { fields: ["url"] },
-          highlightLeftImage: { fields: ["url"] },
-          highlightMidImage: { fields: ["url"] },
-          highlightRightImage: { fields: ["url"] },
-        },
-      },
-      { encodeValuesOnly: true },
-    );
-
-    const path = `/api/global?${query}`;
-    const url = new URL(path, baseUrl);
-
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${process.env.TOKEN}`,
-      },
-      next: { revalidate: 900 },
-    });
-
-    if (!res.ok) {
-      console.error(`Strapi Error: ${res.status} ${res.statusText}`);
-      throw new Error("Failed to fetch page data");
-    }
-
-    const data = await res.json();
-    return flattenAttributes(data.data);
-  } catch (error) {
-    console.error("Fetch Error:", error);
-    throw new Error("Failed to fetch page data");
-  }
-};
 
 const Page = async () => {
   const flattenedData = await getPageData();
+
   const jsonLd = {
     "@context": "https://schema.org",
     "@type": "LocalBusiness",
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
+import qs from "qs";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -56,3 +58,120 @@ export function getStrapiMedia(url: string | null) {
   if (url.startsWith("http") || url.startsWith("//")) return url;
   return `${getStrapiURL()}${url}`;
 }
+
+export const waitForStrapi = async (maxRetries = 5, delay = 5000) => {
+  for (let i = 0; i < maxRetries; i++) {
+    try {
+      const res = await fetch(`${getStrapiURL()}/admin`, {
+        method: "HEAD",
+      });
+      if (res.ok) return true;
+    } catch (e) {
+      console.log(`Strapi is sleeping... retry ${i + 1}/${maxRetries}`);
+    }
+    await new Promise((resolve) => setTimeout(resolve, delay));
+  }
+  throw new Error("Strapi failed to wake up in time.");
+};
+
+export const getPageData = async () => {
+  try {
+    await waitForStrapi();
+    const baseUrl = getStrapiURL();
+
+    const query = qs.stringify(
+      {
+        populate: {
+          introSection: true,
+          Seo: true,
+          introImages: {
+            fields: ["url", "alternativeText", "width", "height", "name"],
+          },
+          promoBanner: { fields: ["url", "alternativeText"] },
+          aboutUsImage: { fields: ["url"] },
+          contactImage: { fields: ["url"] },
+          heroImage: { fields: ["url"] },
+          logo: { fields: ["url"] },
+          highlightLeftImage: { fields: ["url"] },
+          highlightMidImage: { fields: ["url"] },
+          highlightRightImage: { fields: ["url"] },
+        },
+      },
+      { encodeValuesOnly: true },
+    );
+
+    const path = `/api/global?${query}`;
+    const url = new URL(path, baseUrl);
+
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.TOKEN}`,
+      },
+      next: { revalidate: 900 },
+    });
+
+    if (!res.ok) {
+      console.error(`Strapi Error: ${res.status} ${res.statusText}`);
+      throw new Error("Failed to fetch page data");
+    }
+
+    const data = await res.json();
+    return flattenAttributes(data.data);
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    throw new Error("Failed to fetch page data");
+  }
+};
+
+export const getMetadata = async () => {
+  const defaultMetadata: Metadata = {
+    title: "ศรีสุราษฎร์ บ้านน็อคดาวน์",
+    description:
+      "รับสร้างบ้านน็อคดาวน์โครงเหล็ก ราคาเริ่มต้นเพียง 90,000 บาท ออฟฟิศสำเร็จรูป รับต่อเติม รีโนเวท",
+  };
+
+  try {
+    await waitForStrapi();
+    const res = await fetch(
+      `${process.env.STRAPI_API_URL}/api/global?populate[0]=Seo&populate[1]=logo&fields[0]=name&populate[2]=heroImage`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN}`,
+        },
+      },
+    );
+
+    if (!res.ok) throw new Error(`Strapi error: ${res.status}`);
+
+    const json = await res.json();
+    const seo = json.data.Seo;
+    const faviconUrl = json.data.logo.url;
+
+    return {
+      title: seo.metaTitle,
+      description: seo.metaDescription,
+      icons: [{ url: faviconUrl }],
+      metadataBase: new URL(`${process.env.DOMAIN_NAME}`),
+      alternates: { canonical: "/" },
+      robots: "index, follow",
+      openGraph: {
+        title: seo.metaTitle,
+        description: seo.metaDescription,
+        url: process.env.DOMAIN_NAME,
+        siteName: json.data.name,
+        images: [
+          {
+            url: `${process.env.DOMAIN_NAME}/api/og`,
+            width: 1200,
+            height: 630,
+            alt: "Website Preview",
+          },
+        ],
+        type: "website",
+      },
+    };
+  } catch (error) {
+    console.error("Failed to load metadata:", error);
+    return defaultMetadata;
+  }
+};
